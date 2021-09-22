c7305ec29fff041880c30402771d160f8c6ac16d
Britta Grusdt <bgrusdt@gmail.com>
2021-09-22 07:35:16 +0200

    added utils.R, clean_data function

---
+++ R/utils.R
@@ -0,0 +1,24 @@
+FS = .Platform$file.sep
+
+rep_each <- function(x, times) {
+  times <- rep(times, length.out = length(x))
+  rep(x, times = times)
+}
+
+# at most one control-physics trial and none of the control-random trials wrong
+clean_data = function(data){
+  controls = data %>% filter(startsWith(type, "control")) %>% 
+    dplyr::select(c(submission_id, starts_with("id"), type, question,
+                    selected_pic, expected)) %>% 
+    group_by(submission_id, type) %>% 
+    mutate(n=n()) %>% 
+    mutate(correct = selected_pic == expected, n_correct = sum(correct)) %>% 
+    distinct_at(vars(c(type, submission_id)), .keep_all = T)
+  
+  submission_ids.out = controls %>% 
+    filter(!(type == "control-random" & n_correct == n) &
+           !(type == "control-physics" & n_correct >= n-1)) %>% 
+    pull(submission_id) %>% unique()
+  
+  return(data %>% filter(!submission_id %in% submission_ids.out))
+}
