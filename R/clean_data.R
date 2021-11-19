1c48f544a3cfa2ec57b086d1b6c91d94e7385ebd
Britta Grusdt <bgrusdt@gmail.com>
2021-11-19 11:46:57 +0100

    updated file to save cleaned data

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,5 +1,9 @@
+library(here)
+source(here("R", "utils.R"))
 main_data <- read_csv(here("results", "results_67_communicationBlocks_BG.csv"))
 pilot_data <- read_csv(here("results", "results_67_communicationBlocks_BG_pilot.csv"))
+
+# Clean Data --------------------------------------------------------------
 
 # join original pilot data and
 # remove data from our test-runs if had not been done before
@@ -35,11 +39,68 @@ duplicates.out = data.original %>%
 
 data = anti_join(data.cleaned, duplicates.out)
 
+# check for comments
+comments = data %>% select(comments, submission_id) %>% 
+  filter(!is.na(comments) & comments!="") %>% distinct()
+
+comments[11,]$comments
+comments[24,]$comments
+comments[30,]$comments
+
+out.comments = comments[c(11, 24), ] %>% dplyr::select(submission_id)
+
+data = anti_join(data, out.comments)
+
 n_all = data.original$submission_id %>% unique() %>% length
 n_kept = data$submission_id %>% unique() %>% length
 ratio_kept = round(n_kept/n_all, 2)
-
-message(paste("Proportion of data, after cleaning, that will be included in analysis:", 
-              ratio_kept))
-
+msg <- "% of all recorded data after cleaning that is included in analysis:"
+message(paste(msg, ratio_kept))
 write_csv(data, here("results", "data_cleaned.csv"))
+
+
+# numbers removed data ----------------------------------------------------
+data.controls <- data.original %>%
+  group_by(submission_id) %>% 
+  filter(startsWith(type, "control-") | type == "test-example") %>% 
+  mutate(correct = expected == selected_pic) %>% 
+  dplyr::select(correct, type, submission_id, prolific_id, id, timeSpent,
+                comments, startDate) %>% 
+  filter(!str_detect(prolific_id, "test-"))
+
+# 1. Wrong answer in test example
+out.test_ex = data.controls %>% filter(type == "test-example" & !correct) %>%
+  dplyr::select(submission_id, startDate, comments)
+
+# 2. more than 1 control-trial wrong
+out.control = data.controls %>% filter(type == "control-physics") %>% 
+  group_by(submission_id, prolific_id, timeSpent, comments, startDate) %>% 
+  summarize(nb_correct = sum(correct), .groups = "drop_last") %>%
+  filter(nb_correct < 2)
+
+# 3. failed attention-checks
+attention_checks = data.controls %>% filter(type=="control-random")
+out.attention = attention_checks %>%
+  group_by(submission_id, prolific_id, timeSpent, comments, startDate) %>% 
+  summarize(nb_correct = sum(correct), .groups = "drop_last") %>%
+  filter(nb_correct != 3)
+
+# 4. total number of participants removed
+out.n = c(out.test_ex$submission_id, out.control$submission_id, 
+          out.attention$submission_id, out.comments,
+          duplicates.out$submission_id) %>% unique() %>% length()
+
+# Print numbers
+message(paste("#participants who got test-example wrong:", 
+              out.test_ex$submission_id %>% unique() %>% length()))
+message(paste("#participants who got more than 1 control trial wrong:", 
+              out.control$submission_id %>% unique() %>% length()))
+message(paste("#participants who got any of the attention-checks wrong:", 
+              out.attention$submission_id %>% unique() %>% length()))
+message(paste("#participants removed due to several recordings:", 
+              duplicates.out$submission_id %>% unique() %>% length()))
+message(paste("#participants removed due to comments:", 
+              out.comments$submission_id %>% unique() %>% length()))
+
+message(paste("#participants whose data was excluded for the analysis:", out.n))
+
